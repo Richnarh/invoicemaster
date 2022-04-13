5d4fb0b844ca311baceb776051d7bb1689bd64f8
Richard Narh <pascalcppjava@gmail.com>
2022-04-13 18:48:28 +0000

    updates

--- invoice-master/src/main/java/com/khoders/invoicemaster/jbeans/SmsAccess.java
+++ invoice-master/src/main/java/com/khoders/invoicemaster/jbeans/SmsAccess.java
@@ -11,6 +11,8 @@ package com.khoders.invoicemaster.jbeans;
  */
 public class SmsAccess
 {
-  public static  String USERNAME = "pascal";
-  public static  String PASSWORD = "pascal001";
+  public static  String USERNAME = "";
+  public static  String PASSWORD = "";
+//  public static  String USERNAME = "pascal";
+//  public static  String PASSWORD = "pascal001";
 }
